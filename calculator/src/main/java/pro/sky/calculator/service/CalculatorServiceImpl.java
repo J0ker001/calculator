240a1832876b971597840954a36acb540a1d327a
Anton <96440353+J0ker001@users.noreply.github.com>
2022-06-14 22:07:14 +0300

    Update CalculatorServiceImpl.java

--- calculator/src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
+++ calculator/src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
@@ -5,23 +5,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
 
-
+// Sum of two fractional numbers
     @Override
     public double plus(double num1, double num2) {
 
         return num1 + num2;
     }
 
+//Difference of two fractional numbers    
     @Override
     public double minus(double num1, double num2) {
         return num1 - num2;
     }
 
+//Multiplication of two fractional numbers    
     @Override
     public double multiply(double num1, double num2) {
         return num1 * num2;
     }
 
+//Division of two fractional numbers    
     @Override
     public double divide(double num1, double num2) {
         return num1 / num2;
